c0be964b4bb1c8bb4707875027a3b31afe31e899
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-03 18:52:36 +0200

    + part of 1 task to directory lesson7

---
+++ lesson7/task1/first.js
@@ -0,0 +1,80 @@
+
+// let 
+let Some_number = 10
+console.log ('This is some variable of number type-', Some_number, typeof(Some_number))
+
+let Some_string = 'Lorem ipsum dolor sit amet.'
+console.log ('This is some variable of string type-', Some_string, typeof(Some_string))
+
+let Some_boolean = true
+console.log ('This is some variable of boolean type-',Some_boolean, typeof(Some_boolean))
+
+let Some_null = null
+console.log ('This is some Null value-',Some_null, typeof(Some_null) )
+
+let Some_undefined
+console.log ('This is some variable of undefined type-',Some_undefined, typeof(Some_undefined) )
+
+let Some_object = {}
+console.log ('This is some variable of object type-' , Some_object, typeof(Some_object))
+
+let id = Symbol();
+console.log ('This is some symbol with "function type" -', Symbol, typeof(Symbol))
+
+let Big_integer = 2n
+console.log ('This is some variable of bigint type-', Big_integer, typeof(Big_integer))
+
+
+// // ______________________________________________________________________________
+// // var
+// var Some_number = 10
+// console.log ('This is some variable of number type-', Some_number, typeof(Some_number))
+
+// var Some_string = 'Lorem ipsum dolor sit amet.'
+// console.log ('This is some variable of string type-', Some_string, typeof(Some_string))
+// var Some_boolean = true
+// console.log ('This is some variable of boolean type-',Some_boolean, typeof(Some_boolean))
+
+// var Some_null = null
+// console.log ('This is some Null value-',Some_null, typeof(Some_null) )
+
+// var Some_undefined
+// console.log ('This is some variable of undefined type-',Some_undefined, typeof(Some_undefined) )
+
+// var Some_object = {}
+// console.log ('This is some variable of object type-' , Some_object, typeof(Some_object))
+
+// var id = Symbol();
+// console.log ('This is some symbol with "function type" -', Symbol, typeof(Symbol))
+
+// var Big_integer = 2n
+// console.log ('This is some variable of bigint type-', Big_integer, typeof(Big_integer))
+
+// // ______________________________________________________________________________
+// // const
+
+// const Some_number = 10
+// console.log ('This is some variable of number type-', Some_number, typeof(Some_number))
+
+// const Some_string = 'Lorem ipsum dolor sit amet.'
+// console.log ('This is some variable of string type-', Some_string, typeof(Some_string))
+// const Some_boolean = true
+// console.log ('This is some variable of boolean type-',Some_boolean, typeof(Some_boolean))
+
+// const  Some_null = null
+// console.log ('This is some Null value-',Some_null, typeof(Some_null) )
+
+// // const - undefined doesnt aviable to init
+
+
+// console.log ('This is some variable of object type-' , Some_object, typeof(Some_object))
+
+// const  id = Symbol();
+// console.log ('This is some symbol with "function type" -', Symbol, typeof(Symbol))
+
+// const  Big_integer = 2n
+// console.log ('This is some variable of bigint type-', Big_integer, typeof(Big_integer))
+
+// ______________________________________________________________________________
+// 5 examples of conversion number -> string
+
