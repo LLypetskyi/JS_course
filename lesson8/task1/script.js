0dff665ae167a9b6e70f8eb6e10cb3718857c071
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-11 21:11:42 +0200

    add 1 task of lesson8

---
+++ lesson8/task1/script.js
@@ -0,0 +1,15 @@
+// Перевірка на пароль
+
+// Користувача в prompt питають пароль, і якщо цей пароль невірний - викинути повідомлення ‘хибний пароль’
+// Вірний пароль буде ‘787898
+
+const num = prompt("Please enter password - ");
+
+
+if (num === '787898') {
+  console.log("Your password is right");
+} else console.log("Wrong password");
+
+// OR
+
+num === '787898' ? console.log("Your password is right") : console.log("Wrong password");
