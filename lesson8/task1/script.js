35ea01a127a3e2da04a6ec796a15b5e5baefbc42
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-11 23:42:55 +0200

    add 2 task of lesson8

--- lesson8/task1/script.js
+++ lesson8/task1/script.js
@@ -4,8 +4,6 @@
 // Вірний пароль буде ‘787898
 
 const num = prompt("Please enter password - ");
-
-
 if (num === '787898') {
   console.log("Your password is right");
 } else console.log("Wrong password");
