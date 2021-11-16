eef195ad866f5bf8c1fbf7207653dbc1726204b8
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-16 17:43:10 +0200

    add 4 task of lesson9

---
+++ lesson9/task4/script.js
@@ -0,0 +1,13 @@
+// // За допомогою циклів вивести фігуру - прямокутник 10 x 10 - в консоль:
+// Очікуваний результат (скрін з консолі):
+
+
+let size = 10;
+// let rezString="";
+for (let i = 1; i <= size; i++) {
+  let rezString = "";
+  for (let j = 10; j >= i; j--) {
+    rezString = rezString + "*";
+  }
+  console.log(rezString);
+}
