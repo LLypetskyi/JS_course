c3009b77f6a5a11ddecedb7b023d775e3eab9d9a
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-21 16:06:27 +0200

    add aditional task7 of lesson9

---
+++ lesson9/task7AddTriangle/script.js
@@ -0,0 +1,13 @@
+
+
+let size = 6;
+// let i = 0;
+
+while (i <= size) {
+  lineEmpty = "";
+  lineStar = "";
+  for (let j = 0; j < size - i; j++) lineEmpty += " ";
+  for (let k = 0; k < 2 * i + 1; k++) lineStar += "*";
+  console.log(lineEmpty + lineStar);
+  i++;
+}
