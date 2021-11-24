20f3d3b371a6a421ef1f5225b6bd3a9f4f179440
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-24 17:16:00 +0200

    add  changes to addition task6 of lesson10

--- lesson10/task6random/script.js
+++ lesson10/task6random/script.js
@@ -2,14 +2,17 @@
 // Ти повинен вгадати, яке число загадав комп'ютер ввівши його в відповідному prompt().
 // Після цього комп'ютер говорить тобі чи ти вгадав загадане ним число чи ні
 
-let userNumber = prompt("Please guess the number from 1 to 10");
+let userNumber = +prompt("Please guess the number from 1 to 10");
 
-let randNumber = function randomNumber() {
-  let rez = Math.floor(Math.random() * 10) + 1;
-  return rez;
-};
-
-console.log("Сomputer selected number - " + randNumber());
-if (randNumber() === userNumber) {
+// let randNumber = 
+function randomNumber() {
+  let rez = Math.floor(Math.random() * 11);
+console.log("Сomputer selected number - " + rez);
+if (rez === userNumber) {
   alert("Сongratulations! You guessed the number");
 } else alert("Unfortunately you didn't guess");
+};
+
+randomNumber();
+
+
