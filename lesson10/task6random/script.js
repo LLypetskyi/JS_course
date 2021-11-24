df5b58e35c2348dbe038ee99775b5635096eb9b7
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-24 16:50:50 +0200

    add  addition task6 of lesson10

---
+++ lesson10/task6random/script.js
@@ -0,0 +1,15 @@
+// * Створіть функцію, яка загадує рандомне число від 1 до 10.
+// Ти повинен вгадати, яке число загадав комп'ютер ввівши його в відповідному prompt().
+// Після цього комп'ютер говорить тобі чи ти вгадав загадане ним число чи ні
+
+let userNumber = prompt("Please guess the number from 1 to 10");
+
+let randNumber = function randomNumber() {
+  let rez = Math.floor(Math.random() * 10) + 1;
+  return rez;
+};
+
+console.log("Сomputer selected number - " + randNumber());
+if (randNumber() === userNumber) {
+  alert("Сongratulations! You guessed the number");
+} else alert("Unfortunately you didn't guess");
