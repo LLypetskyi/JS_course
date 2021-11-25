be1594f5d3c3d86dc93fd244d160ef1a67cbedd2
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-26 00:40:47 +0200

    add changes to  addition task7 of lesson10

--- lesson10/task7getColors/script.js
+++ lesson10/task7getColors/script.js
@@ -2,5 +2,13 @@
 // Поміщає їх в блоки. Користувач має обрати колір на клік. 
 // І після того комп'ютер показує який колір був вибраний
 
+const button = document.querySelector('button');
+let fon = document.querySelector('wrapper');
+// const colorCode = document.querySelector ('') Куди передавати код по "кліку по блоку"?;
 
-
+button.addEventListener('click',() =>{
+  let color = '#';
+  color += Math.random().toString(16).slice(2,8);
+  console.log(color);
+  fon.style.backgroundColor = color;
+})
