b9e28a8534329d227a5e16c18625e867cff41f28
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-27 23:24:33 +0200

    add changes to  addition task7 of lesson10

--- lesson10/task7getColors/script.js
+++ lesson10/task7getColors/script.js
@@ -6,27 +6,30 @@ const button = document.querySelector('button');
 const fon = document.querySelector('wrapper');
 const elements = document.querySelector('.cub');
 
-// функція рандомайзер кольору
+
+// function for to get random color
 function randomColor() {
   let color = '#';
   color += Math.random().toString(16).slice(2,8);
   console.log(color);
+};
+
+// then we click on button we change color
+let newColor = button.addEventListener('click', randomColor);
+
+
+// маємо пройти по всіх div з класом cub і задати стиль бекграунду 
+for (let elem of elements){ 
+//  добавляємо в css колір бекраунду
+elements.style.backgroundColor = 'red'; // Функція підбору кольору
+console.log (elements.style.backgroundColor);
 }
 
 
-button.addEventListener('click', randomColor()); //прикліку змінється код кольору
-elements.style.background-color = function randomColor(); // Функція підбору кольору
-
-// for (let elem of elements ){}
-//   // for (let i=1; i<=element.length; i++){
 //   // element(бекшраундКолор) = color;
 //   // }}
   
 
 
-
-  // button.addEventListener("click", function(){ alert("Hello World!"); }); 
-// добавляємо в css колір бекраунду
-  // elements.style.background-color = function randomColor(); // Функція підбору кольору
 
 
