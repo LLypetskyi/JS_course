316f255f5606506b35fb3cb400d9e2fa179b5d2d
unknown <liubomyr.lypetskyi@gmail.com>
2021-11-29 13:43:57 +0200

    add final changes to addition task7 of lesson10

--- lesson10/task7getColors/script.js
+++ lesson10/task7getColors/script.js
@@ -3,14 +3,12 @@
 // І після того комп'ютер показує який колір був вибраний
 
 const button = document.querySelector(".button");
-// const fonBody = document.body;
-const fonBody = document.getElementsByClassName('body');
+const fonBody = document.getElementsByClassName("body");
 const elements = document.getElementsByClassName("cub");
-
 
 // then we click on button we change color
 let newColor = button.addEventListener("click", () => {
-setRandomColor()
+  setRandomColor();
 });
 
 // function for to get random color
@@ -18,22 +16,19 @@ function createRandomColor() {
   let color = "#";
   color += Math.random().toString(16).slice(2, 8);
   return color;
-  // console.log(color);
 }
 
 function setRandomColor() {
-  let gradient = 'linear-gradient(to right, ';
+  let gradient = "linear-gradient(to right";
   // маємо пройти по всіх div з класом cub і задати стиль бекграунду
   for (let elem of elements) {
     elem.style.background = createRandomColor(); //  добавляємо в css кожному <div> колір бекраунду
-    gradient = gradient + elem.style.background + ',' + ' ';  
+    gradient = gradient + "," + elem.style.background + " ";
   }
 
-  gradient = gradient + 'rgb(85, 170, 187)' + ')'+ ';';
+  gradient = gradient + ")";
   console.log(gradient);
-  console.log(typeof(gradient));
+  console.log(typeof gradient);
   document.body.style.background = gradient;
-  // fonBody[0].style.background = gradient;
-  console.log (typeof(gradient));
+  console.log(typeof gradient);
 }
-console.log(fonBody);
