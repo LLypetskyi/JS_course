8813f38500a941dee712bcb1f9da91706b493bcd
unknown <liubomyr.lypetskyi@gmail.com>
2021-12-02 01:08:06 +0200

    add part of task2 of lesson11

---
+++ lesson11/task2/script.js
@@ -0,0 +1,35 @@
+// // Створіть функцію, яка додає 3 цифри і виводить результат в перемінну, в консоль, в алерт
+
+console.log(" ----------------------------------");
+
+let user = {
+  name: "Liubomyr",
+  surname: "Lypetskyi",
+  age: 34,
+  sex: "male",
+  married: true,
+  children: true,
+  adress: {
+    cityIndex: "79000",
+    city: "Lviv",
+    street: "Striyska",
+    building: "204",
+    room: 44,
+  }
+  cellFone: "0978497686",
+  mail: "liubomyr.lypetskyi@gmail.com",
+  "driver's license": true,
+  education: "Master of Economics",
+};
+
+console.info(user);
+
+let copyUser = {};
+// Створюєм функцію для копіювання одного ОБ'ЄКТА В ІНШИЙ
+let function createUserCopy(user, copyUser) {
+  for (let key in user) {
+    
+  }
+
+
+}
