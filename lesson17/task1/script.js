4c7bbff7b6d8bb90d2b5ffbce031771d4e409a08
unknown <liubomyr.lypetskyi@gmail.com>
2022-02-05 20:12:48 +0200

    add task to lesson17

--- lesson17/task1/script.js
+++ lesson17/task1/script.js
@@ -1,47 +1,53 @@
 const title = 'Some blog';
 const titleElement = document.querySelector('#title');
 titleElement.innerHTML = title;
-// titleElement.classList.add('title');
 
 const data = [
   {
     title: 'Hello',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed finibus ex. Maecenas egestas nunc eu ipsum sodales porttitor. Orci varius natoque penatibus et magnis.',
+    comments: ['fComment', 'SComment'],
   },
 
   {
     title: 'Hello',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed finibus ex. Maecenas egestas nunc eu ipsum sodales porttitor. Orci varius natoque penatibus et magnis.',
+    comments: [],
   },
 ];
 
 const mainContainer = document.querySelector('.mainContainer');
 
-data.forEach((item) => {
+function render() {
+  mainContainer.innerHTML = '';
+  data.forEach((item) => {
+    createPost(item.title, item.desc);
+  });
+}
+
+const addPostForm = document.querySelector('#addNewPost');
+
+addPostForm.addEventListener('submit', (Event) => {
+  Event.preventDefault();
+  const titleInput = document.querySelector('#titleInput').value;
+  const descInput = document.querySelector('#descInput').value;
+  data.push({ title: titleInput, desc: descInput, comments: [] });
+  render();
+});
+
+function createPost(title, desc) {
   const postElement = document.createElement('div');
   postElement.classList.add('post');
 
   const newH2 = document.createElement('h2');
-  newH2.innerHTML = item.title;
+  newH2.innerHTML = title;
   postElement.appendChild(newH2);
 
   const newDesc = document.createElement('p');
-  newDesc.innerHTML = item.desc;
+  newDesc.innerHTML = desc;
   postElement.appendChild(newDesc);
 
   mainContainer.appendChild(postElement);
-});
+}
 
-const addPostForm = document.querySelector('#addNewPost');
-const titleInput = document.querySelector('#titleInput');
-const descInput = document.querySelector('#descInput');
-
-addPostForm.addEventListener('submit', (Event) => {
-  data.push({
-    title: titleInput.value,
-    desc: descInput.value,
-  });
-
-  console.log('data', data);
-  Event.preventDefault();
-});
+render();
