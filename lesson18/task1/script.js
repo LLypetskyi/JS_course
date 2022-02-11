6a65b2eb5b4d5dc9685fc0a4e7160d0163a7ad1d
unknown <liubomyr.lypetskyi@gmail.com>
2022-02-11 22:53:10 +0200

    add part of task to lesson18

--- lesson18/task1/script.js
+++ lesson18/task1/script.js
@@ -1,27 +1,18 @@
 const battlefield = document.querySelector('#battlefield');
 // console.log(battlefield); ok Отримали все ігрове поле
 let step = 0;
-// лічильник ходу (парний = ставимо Х, непарний - О)
-// виграшні варіанти комбінацій
-// const winCombination = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 4, 8],
-//   [2, 4, 6],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-// ];
+
 battlefield.addEventListener('click', (event) => {
   if ((event.target.className = 'field')) {
-    if (step % 2 === 0 && event.target.innerHTML == '') {
-      event.target.innerHTML = 'X';
-    } else {
-      event.target.innerHTML = '0';
+    if (!event.target.innerHTML) {
+      if (step % 2 === 0 && event.target.innerHTML == '') {
+        event.target.innerHTML = 'X';
+      } else {
+        event.target.innerHTML = '0';
+      }
+      step++;
+      check();
     }
-    step++;
-    check();
   }
 });
 
@@ -37,20 +28,20 @@ const check = function () {
     [1, 4, 7],
     [2, 5, 8],
   ];
-
+  // let winArr = [];
   for (let i = 0; i < winCombination.length; i++) {
     if (
       gameFields[winCombination[i][0]].innerHTML == 'X' &&
       gameFields[winCombination[i][1]].innerHTML == 'X' &&
       gameFields[winCombination[i][2]].innerHTML == 'X'
     )
-      alert('X - win!!!!');
+      alert('X - win!!!');
     else if (
       gameFields[winCombination[i][0]].innerHTML == '0' &&
       gameFields[winCombination[i][1]].innerHTML == '0' &&
       gameFields[winCombination[i][2]].innerHTML == '0'
     )
-      alert('0 - win!!!!');
-    // else alert('friendship won!!!!');
+      alert('0 - win!!!');
+    // else if ((i = 9)) alert('friendship won!!!!');
   }
 };
