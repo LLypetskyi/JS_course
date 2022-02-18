de228b33b15b15e83ca0a6d4b2c64061cbb59bb6
unknown <liubomyr.lypetskyi@gmail.com>
2022-02-18 23:06:40 +0200

    add same part of task1 to Angular lesson

---
+++ Angular/hw1/homeWork/src/app/components/pipe/pipe.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe',
+  templateUrl: './pipe.component.html',
+  styleUrls: ['./pipe.component.css']
+})
+export class PipeComponent {
+
+  constructor() { }
+  dataVar = Date();
+  currency = 200;
+  percent = 0.20;
+  upperCaseStr = 'hello from parent component'
+  lowerCaseStr = 'HELLO FROM PARENT COMPONENT'
+  sliceStr = 'цю стрінгу потрібно обрізати'
+
+
+
+}
