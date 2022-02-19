f34952f21d3dfa31ade1decf05d57eea7f43e334
unknown <liubomyr.lypetskyi@gmail.com>
2022-02-19 17:49:19 +0200

    add same part of task1 to Angular lesson

--- Angular/hw1/homeWork/src/app/app.module.ts
+++ Angular/hw1/homeWork/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PipeComponent } from './components/pipe/pipe.component';
 import { DirectiveForComponent } from './components/directiveFor/directive-for/directive-for.component';
 import { DirectiveIfComponent } from './components/directiveIf/directive-if/directive-if.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipeComponent,
     DirectiveForComponent,
-    DirectiveIfComponent
+    DirectiveIfComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/hw1/homeWork/src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+arr: string [] = ['bread', 'butter', 'milk', 'kefir', 'cheese','fish', 'chicken', 'apples', 'bananas', 'candies']
+
+  constructor() { }
+
+  
+
+}
